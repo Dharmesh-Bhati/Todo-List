fd328c453466dae0e8c2d23ce57cd9c8a73451af
Dharmesh Bhati <dharmeshbhati62@gmail.com>
2025-07-21 18:01:52 +0530

    Fixed CORS for Render deployment

--- Server/app.js
+++ Server/app.js
@@ -6,7 +6,9 @@ const todoRoutes = require('./routes/todoRoutes')
 const dotenv = require('dotenv').config()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+   origin: ["http://localhost:5173", "https://mern-todo-list-hfm7.onrender.com"]
+}))
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('Connected!'))
